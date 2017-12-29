5194a0cb4426840ea624d004d412d26da2ba9c25
Oliver Wolf <root@streacs.com>
2017-12-30 00:39:45 +0100

    added more directory tests

--- rspec-specs/spec/localhost/server_spec.rb
+++ rspec-specs/spec/localhost/server_spec.rb
@@ -6,3 +6,15 @@ describe file('/home/bitbucket') do
   it { should be_owned_by 'bitbucket' }
   it { should be_grouped_into 'bitbucket' }
 end
+
+describe file('/opt/atlassian/bitbucket') do
+  it { should be_directory }
+  it { should be_owned_by 'bitbucket' }
+  it { should be_grouped_into 'bitbucket' }
+end
+
+describe file('/var/opt/atlassian/application-data/bitbucket') do
+  it { should be_directory }
+  it { should be_owned_by 'bitbucket' }
+  it { should be_grouped_into 'bitbucket' }
+end
