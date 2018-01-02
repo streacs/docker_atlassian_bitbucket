4868e25cd80c5e3e159cd5258f0074347c41beef
Oliver Wolf <root@streacs.com>
2018-01-02 23:00:56 +0100

    expected fix for : Plan import is blocked to prevent deleting your plan plugin configurations silently

--- bamboo-specs/src/main/java/com/streacs/java/PlanSpec.java
+++ bamboo-specs/src/main/java/com/streacs/java/PlanSpec.java
@@ -67,6 +67,7 @@ public class PlanSpec {
             project(),
             "STREACS Atlassian Bitbucket", "CB95EA")
             .enabled(true)
+            .noPluginConfigurations()
             .noNotifications()
             .linkedRepositories("DCK - STREACS Atlassian Bitbucket (master)")
             .planBranchManagement(new PlanBranchManagement()
